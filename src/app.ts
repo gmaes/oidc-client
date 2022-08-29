81ffa7b8dca96bdc1a9c9158bd6eb1ca53f3a5a6
mduval <maxime@kili-technology.com>
2022-08-29 14:41:39 +0200

    feat: add logging

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ Usage
 Start: 
 npm run start
 Go to http://localhost:3001/authentication -> redirect to sign page from server -> authorize -> Strategy
+// http://localhost:3001/authentication2
 */
 
 import express, { Express } from "express";
@@ -12,7 +13,7 @@ import expressSession from "express-session";
 import { Issuer, Strategy, TokenSet } from "openid-client";
 import passport from "passport";
 import { create as createRootCas } from "ssl-root-cas";
-import OpenIDConnectStrategy from "passport-openidconnect";
+import OpenIDConnectStrategy from "./strategy";
 
 const local = true;
 let autoIssuerUrl;
@@ -26,7 +27,7 @@ if (local) {
   rejectNewUsers = false;
 } else {
   autoIssuerUrl = "";
-  clientId = "KILi";
+  clientId = "KILI";
   clientSecret = "";
   rejectNewUsers = false;
 }
@@ -87,76 +88,10 @@ const openIdConnectStrategy = async (app: Express) => {
   }
   try {
     const issuer = await Issuer.discover(autoIssuerUrl);
-    console.log(`To authenticate, go to http://localhost:3001/authentication`);
-    const client = new issuer.Client({
-      client_id: clientId,
-      client_secret: clientSecret,
-      redirect_uris: [redirectURI],
-      // response_types: ['code'],
-      token_endpoint_auth_method: clientSecret ? "client_secret_post" : "none",
-    });
+    console.log(`To authenticate, go to http://localhost:3001/authentication2`);
 
     app.use(passport.initialize());
     app.use(passport.session());
-
-    passport.use(
-      "oidc",
-      new Strategy(
-        { client },
-        async (tokenSet: TokenSet, userInfo: any, done: any) => {
-          console.log("USE STRAT");
-          const claims = tokenSet.claims();
-          console.log("use strategry oidc", {
-            claims,
-            rejectNewUsers,
-            tokenSet,
-            userInfo,
-          });
-          if (!rejectNewUsers) {
-            const scopes = (tokenSet.scope ?? "").split(" ");
-            const isEmailInScope = scopes.includes("email");
-            if (isEmailInScope) {
-              const email = userInfo.email;
-              console.log("Email in scope and got email", { email });
-              // signUpUserIfNotExists(models, email).then((_) => {
-              const claims = tokenSet.claims();
-              const fullObject = {
-                ...userInfo,
-                ...claims,
-              };
-              return done(null, fullObject);
-              // });
-            }
-            const { access_token, token_type } = tokenSet;
-            console.log({ access_token, token_type });
-            if (access_token) {
-              client
-                .userinfo(access_token, {
-                  method: "POST",
-                  params: { email: null },
-                  tokenType: token_type,
-                  via: "header",
-                })
-                .then((userInfoResponse) => {
-                  const email = userInfoResponse.email;
-                  console.log({ email, userInfoResponse });
-                  if (email) {
-                    const fullObject = {
-                      ...userInfoResponse,
-                    };
-                    return done(null, fullObject);
-                  }
-                  return done(null, tokenSet.claims());
-                })
-                .catch((error) => console.error(error));
-            }
-          } else {
-            console.log("reject new users");
-            return done(null, tokenSet.claims());
-          }
-        }
-      )
-    );
 
     passport.serializeUser((user: any, done: any) => {
       console.log("searialize", { user });
@@ -168,80 +103,35 @@ const openIdConnectStrategy = async (app: Express) => {
       done(null, user);
     });
 
-    app.get(fullPath(AuthRoute.AuthenticationStart), (req, res, next) => {
-      // const scope = (issuer.metadata?.scopes_supported as string[]).join(' ') ?? 'openid email';
-      console.log("START");
-      const scope = "openid";
-      // const scope = "openid";
-      console.log({ scope });
-      passport.authenticate("oidc", { scope })(req, res, next);
-    });
-
-    app.get(fullPath(AuthRoute.AuthenticationCallback), (req, res, next) => {
-      console.log("CALLBACK");
-      passport.authenticate("oidc", {
-        failureRedirect: getFrontendEndpoint(),
-        scope: "openid",
-        successRedirect: `${getFrontendEndpoint()}/label/login/success`,
-      })(req, res, next);
-    });
-
-    const corsOptions = {
-      credentials: true,
-      origin: true,
-    };
-
-    app.get(fullPath(AuthRoute.SignIn), cors(corsOptions), async (req, res) => {
-      // @ts-ignore
-      if (!req.isAuthenticated()) {
-        res.redirect(getFrontendEndpoint());
+    const strategy = new OpenIDConnectStrategy(
+      {
+        issuer: "http://localhost:5770/",
+        authorizationURL: "http://localhost:5770/auth",
+        tokenURL: "http://localhost:5770/token",
+        userInfoURL: "http://localhost:5770/me",
+        clientID: clientId,
+        clientSecret: clientSecret,
+        callbackURL: redirectURI2,
+      },
+      // {
+      //   issuer: "https://identityrec.devinfo.fr.cly/outil/SOID/",
+      //   authorizationURL:
+      //     "https://identityrec.devinfo.fr.cly/outil/SOID/openid/authorize",
+      //   tokenURL: "https://identityrec.devinfo.fr.cly/outil/SOID/openid/token",
+      //   userInfoURL:
+      //     "https://identityrec.devinfo.fr.cly/outil/SOID/openid/userinfo",
+      //   clientID: clientId,
+      //   clientSecret: clientSecret,
+      //   callbackURL: redirectURI2,
+      // },
+      function verify(issuer, profile, cb) {
+        console.log("VERIFY", { profile });
+        const user = { email: "test+admin@kili-technology.com" };
+        return cb(null, user);
       }
-      // @ts-ignore
-      const email = req.user?.email as string;
-      // @ts-ignore
-      console.log("USE SIGNIN", req.user);
-      const user = await findByEmail(email);
-      if (!user && rejectNewUsers) {
-        return res.sendStatus(403);
-      }
-      const payload = {
-        id: user.id,
-        token: await createToken(user, "4w"),
-        user,
-      };
-      return res.json(payload);
-    });
-
-    app.get(fullPath(AuthRoute.LogOutStart), (_, res) => {
-      res.redirect(client.endSessionUrl());
-    });
-
-    app.get(fullPath(AuthRoute.LogOutCallback), (req, res) => {
-      // @ts-ignore
-      req.logout();
-      res.redirect(getFrontendEndpoint());
-    });
-
-    passport.use(
-      new OpenIDConnectStrategy(
-        {
-          issuer: "https://identityrec.devinfo.fr.cly/outil/SOID/",
-          authorizationURL:
-            "https://identityrec.devinfo.fr.cly/outil/SOID/openid/authorize",
-          tokenURL:
-            "https://identityrec.devinfo.fr.cly/outil/SOID/openid/token",
-          userInfoURL:
-            "https://identityrec.devinfo.fr.cly/outil/SOID/openid/userinfo",
-          clientID: clientId,
-          clientSecret: clientSecret,
-          callbackURL: redirectURI2,
-        },
-        function verify(issuer, profile, cb) {
-          const user = { email: "test+admin@kili-technology.com" };
-          return cb(null, user);
-        }
-      )
     );
+
+    passport.use(strategy);
 
     app.get(
       fullPath(AuthRoute2.AuthenticationStart),
